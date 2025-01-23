1decb5951db364a250ad820759fbe276dc3172d9
MarcoAmantea <marcoamantea18@gmail.com>
2025-01-23 18:04:03 +0100

    feat: index and show routers

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,12 +1,44 @@
+const dbConnection = require("../data/db-connection");
+
 const index = (req, res) => {
-    res.json({
-        message: "index di film"
-    })
+   const sql = "SELECT * FROM movies"
+
+   dbConnection.query(sql, (err, movies) =>{
+    if(err){
+        return res.status(500).json({
+            message: "Errore interno del server"
+        })
+    }else {
+        return res.status(200).json({
+            status: "success",
+            data: movies,
+        })
+    }
+    
+   })
 }
 
 const show = (req, res) => {
-    res.json({
-        message: "show dei film"
+    const id = req.params.id;
+    console.log('id', id);
+    
+    const sql = `SELECT * FROM movies WHERE id = ? `;
+    dbConnection.query(sql, [id], (err, movies) => {
+        if(err) {
+            return res.status(500).json({
+            message: "errore interno del server"
+        })
+    }
+    if (movies.length === 0) {
+        return res.status(404).json({
+            message: "film non trovato"
+        })
+    }else {
+        return res.status(200).json({
+            status: "success",
+            data: movies[0]
+        })
+    }
     })
 }
 
