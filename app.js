546e501dded52ddf37c5968af76e5cb6218e601c
MarcoAmantea <marcoamantea18@gmail.com>
2025-01-23 17:19:14 +0100

    feat: file env.example and movies database

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const movieRouter = require("./routers/movies")
+const movieRouter = require("./routers/movies");
+const connection = require("./data/db-connection");
+
 //creazione app express
 const app = express();
 
@@ -9,7 +11,7 @@ const port = process.env.SERVER_PORT;
 //definisco le rotte
 app.use("/movies", movieRouter);
 
-
+//controlliamo se il server è connesso
 app.listen(port, () => {
     console.log(`SERVER M SIEEENT SULLA PORTA ${port}?? T'APPOOOOOST!`);    
 })
